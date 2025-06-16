6d8d1ff0805109b05d0f1957cde550a44f3000d4
GabrielSilvaRodrigues1 <gabrielsilvagithub@gmail.com>
2025-06-16 13:49:51 +0000

    feat: atualizar menu de acessibilidade com novas opções de cadastro e login

--- public/usuario/nutricionista/header.php
+++ public/usuario/nutricionista/header.php
@@ -22,16 +22,20 @@
             </label>
             <div class="overlay">
                 <nav>
-                    <ul>
-                        <li><a href="../../about.php" class="link">Sobre Nós</a></li>
-                        <li><a href="../../calculo_landpage.php" class="link">Cálculo nutricional</a></li>
-                        <li><a href="conta.php" class="link">Conta</a></li>
-                        <li><a href="../sair_usuario.php" class="link">Sair</a></li>
-                        <nav aria-label="Acessibilidade" class="acessibilidade">
-                            <button onclick="toggleContraste()" id="contraste-btn" aria-pressed="false" aria-label="Ativar ou desativar alto contraste">Alto Contraste</button>
-                            <button onclick="aumentarFonte()" id="aumentar-fonte-btn" aria-label="Aumentar tamanho da fonte" accesskey="2" tabindex="2">A+</button>
-                            <button onclick="diminuirFonte()" id="diminuir-fonte-btn" aria-label="Diminuir tamanho da fonte" accesskey="3" tabindex="3">A-</button>
-                        </nav>
+                    <ul aria-label="Acessibilidade" class="acessibilidade">
+                        <li><a href="about.php" class="link">Sobre Nós</a></li>
+                        <li><a href="calculo_landpage.php" class="link">Cálculo nutricional</a></li>
+                        <li><a href="cadastrar_usuario.php" class="link">Cadastrar-se</a></li>
+                        <li><a href="entrar_usuario.php" class="link">Login</a></li>
+                        <li><button onclick="toggleContraste()" id="contraste-btn" aria-pressed="false" aria-label="Ativar ou desativar alto contraste">Alto Contraste</button></li>
+                        <li><p>Modificar tamanho da fonte</p></li>
+                        <li><button onclick="aumentarFonte()" id="aumentar-fonte-btn" aria-label="Aumentar tamanho da fonte" accesskey="2" tabindex="2">A+</button></li>
+                        <li><button onclick="diminuirFonte()" id="diminuir-fonte-btn" aria-label="Diminuir tamanho da fonte" accesskey="3" tabindex="3">A-</button></li>
+                        <li><p>Modificar estilo da exibição</p></li>
+                        <li><button onclick="toggleDaltonismo('protanopia')" aria-label="Simular protanopia">Protanopia</button></li>
+                        <li><button onclick="toggleDaltonismo('deuteranopia')" aria-label="Simular deuteranopia">Deuteranopia</button></li>
+                        <li><button onclick="toggleDaltonismo('tritanopia')" aria-label="Simular tritanopia">Tritanopia</button></li>
+                        <button onclick="resetarAcessibilidade()" aria-label="Restaurar configurações de acessibilidade">Voltar ao normal</button>
                     </ul>
                 </nav>
             </div>
