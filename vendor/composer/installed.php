3e4fc23a270d676d877ad24ba8fe39569505beef
GabrielSilvaRodrigues1 <gabrielsilvagithub@gmail.com>
2025-06-16 21:23:10 +0000

    Add initial Composer setup and autoloading configuration
    - Created .gitignore file to exclude unnecessary files and directories from version control.
    - Added vendor/autoload.php for Composer autoloading functionality.
    - Implemented ClassLoader in vendor/composer/ClassLoader.php to handle PSR-0 and PSR-4 autoloading.
    - Introduced InstalledVersions.php to manage installed package versions.
    - Included LICENSE file for Composer's licensing information.
    - Generated autoload classmap, namespaces, PSR-4, and real autoload files for proper autoloading.
    - Created installed.php and installed.json to track installed packages and their versions.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'delimeter/mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2743c023cf8e55c27dadc50138f3cdf88fe6c06a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'delimeter/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2743c023cf8e55c27dadc50138f3cdf88fe6c06a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
